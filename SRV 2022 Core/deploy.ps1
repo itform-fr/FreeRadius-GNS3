ed339773c70a56fe924cd21ede5d58f280ac34fd
itform-fr <131718750+itform-fr@users.noreply.github.com>
2025-03-11 18:42:42 +0100

    Create deploy.ps1

---
+++ SRV 2022 Core/deploy.ps1
@@ -0,0 +1,9 @@
+$Carte=$(Get-NetAdapter).name
+$Adresse="192.168.50.250"
+$Passerelle="192.168.50.254"
+$Domaine="ais.labo"
+Set-NetIPInterface -InterfaceAlias $Carte -Dhcp Disabled
+New-NetIPAddress -InterfaceAlias $Carte -IPAddress $Adresse -PrefixLength 24 -DefaultGateway $Passerelle
+Install-WindowsFeature ad-domain-services
+Install-ADDSForest -DomainName $Domaine -SafeModeAdministratorPassword $(ConvertTo-SecureString -Force -AsPlainText "0Poseidon")  -confirm:$false
+
