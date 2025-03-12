0b0253c5417d1f78116fc45e2d1b62bdee8b888a
itform-fr <131718750+itform-fr@users.noreply.github.com>
2025-03-12 09:17:36 +0100

    Update deploy.ps1

--- SRV 2022 Core/deploy.ps1
+++ SRV 2022 Core/deploy.ps1
@@ -7,7 +7,7 @@ $Domaine="ais.labo"
 Set-NetIPInterface -InterfaceAlias $Carte -Dhcp Disabled
 New-NetIPAddress -InterfaceAlias $Carte -IPAddress $Adresse -PrefixLength 24 -DefaultGateway $Passerelle
 #>
-New-ItemProperty -Path "HKLM\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters" -Name "AlwaysExpectDomainController" -Value 1 -PropertyType DWord -Force
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters" -Name "AlwaysExpectDomainController" -Value 1 -PropertyType DWord -Force
 Install-WindowsFeature ad-domain-services
 Install-ADDSForest -DomainName ais.labo -SafeModeAdministratorPassword $(ConvertTo-SecureString -Force -AsPlainText "0Poseidon")  -confirm:$false
 
