3a2e1416b2212b930a4837c4b959467ceff925ea
itform-fr <131718750+itform-fr@users.noreply.github.com>
2025-03-12 10:05:19 +0100

    Update config.sh

--- FreeRadius/config.sh
+++ FreeRadius/config.sh
@@ -25,7 +25,7 @@ cat << EOF > /etc/samba/smb.conf
    realm = AIS.LABO
 EOF
 
-sed -e '/^#NTP/ s/^#//;/^NTP/ s/=/=192.168.50.250/;/^#RootD/ s/^#//;/^RootD/ s/5/100/' /etc/systemd/timesyncd.conf
+sed -i '/^#NTP/ s/^#//;/^NTP/ s/=/=192.168.50.250/;/^#RootD/ s/^#//;/^RootD/ s/5/100/' /etc/systemd/timesyncd.conf
 systemctl enable --now systemd-timesyncd
 
 net ads join --no-dns-updates -U administrateur%0Poseidon
