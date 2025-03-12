323c0dbb68b130d42af83dc18fca1100e8561f52
itform-fr <131718750+itform-fr@users.noreply.github.com>
2025-03-12 14:15:35 +0100

    Update config.sh

--- FreeRadius/config.sh
+++ FreeRadius/config.sh
@@ -1,7 +1,7 @@
 apt update && apt install freeradius samba winbind krb5-user freeradius-ldap -y
 adduser freerad winbindd_priv
 
-sed -i '/#bob/ s/^#//;s/hello/poseidon/' /etc/freeradius/3.0/users
+#sed -i '/#bob/ s/^#//;s/hello/poseidon/' /etc/freeradius/3.0/users
 sed -i '$ a client SW-L3 {\n\tipaddr = 192.168.100.254\n\tsecret = poseidon\n}' /etc/freeradius/3.0/clients.conf
 
 echo "nameserver 192.168.50.250" > /etc/resolv.conf
@@ -31,3 +31,13 @@ systemctl enable --now systemd-timesyncd
 net ads join --no-dns-updates -U administrateur%0Poseidon
 systemctl restart winbind
 wbinfo -u
+
+pushd /etc/freeradius/3.0/certs
+chmod +x bootstrap
+./bootstrap
+chown freerad:freerad *
+popd
+sed -i '/private_key_file/ s/=.*/= \/etc\/freeradius\/3.0\/certs\/server.key/;/certificate_file/ s/=.*/=\/etc\/freeradius\/3.0\/certs\/server.pem/;/ca_file/ s/=.*/=\/etc\/freeradius\/3.0\/certs\/ca.pem/;/use_tunneled_reply/,+1 s/= no/= yes/' eap
+sed -i '/#use_mppe/ s/#//;/use_mppe/ s/no/yes/;/#require_encryption/ s/#//;/require_encryption/ s/no/yes/;/#require_strong/ s/#//;/require_strong/ s/no/yes/;/#winbind_username/ s/#//;/#winbi
+nd_domain/ s/#//' mschap
+
