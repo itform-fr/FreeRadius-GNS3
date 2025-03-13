edb25ad172f60e172135cfaca61bf6c843fcacaf
itform-fr <131718750+itform-fr@users.noreply.github.com>
2025-03-13 18:02:00 +0100

    Update deploy.ps1

--- SRV 2022 Core/deploy.ps1
+++ SRV 2022 Core/deploy.ps1
@@ -7,6 +7,7 @@ $Domaine="ais.labo"
 Set-NetIPInterface -InterfaceAlias $Carte -Dhcp Disabled
 New-NetIPAddress -InterfaceAlias $Carte -IPAddress $Adresse -PrefixLength 24 -DefaultGateway $Passerelle
 #>
+curl -o c:\configure.ps1 https://raw.githubusercontent.com/itform-fr/FreeRadius-GNS3/refs/heads/main/SRV%202022%20Core/Configure.ps1
 Install-WindowsFeature ad-domain-services
 Install-ADDSForest -DomainName ais.labo -SafeModeAdministratorPassword $(ConvertTo-SecureString -Force -AsPlainText "0Poseidon")  -confirm:$false
 
